39049829a2a5065f22b33ca63548c1a50ba9d571
Farzad <farzad@MacBookPro.attlocal.net>
2025-02-24 19:28:11 -0800

    Add stock price simulator with geometric Brownian motion and options pricing

---
+++ price_simulator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Stock Price Simulator Package
+
+This package provides tools for simulating stock price movements and options pricing.
+"""
+
+from .price_simulator import StockPriceSimulator
+
+__all__ = ['StockPriceSimulator']
