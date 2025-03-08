fb7ad921c0f72c419d3c54b4ada7b66b2a8a9936
Maharshi Patel <maharshi.patel@airbnb.com>
2025-03-08 12:15:07 -0800

    add agent state

--- policy_interface.py
+++ policy_interface.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 from portfolio import PortfolioState
+from agent import AgentState
 
 class Policy(ABC):
 
     @abstractmethod
-    def get_action(self, state: PortfolioState):
+    def get_action(self, state: AgentState):
         pass
     
